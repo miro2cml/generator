cf26cf24a5a91b1cb247b962512cda7a51636aab
Timothée Moos <t1997.m@gmx.net>
2020-10-16 10:05:43 +0200

    add basic getBoard method

---
+++ src/main/java/ch/ost/rj/sa/miro2cml/data_access/MiroApiConsumer.java
@@ -0,0 +1,28 @@
+package ch.ost.rj.sa.miro2cml.data_access;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+public class MiroApiConsumer {
+    private static void getBoard(String accessToken, String boardID) {
+
+        String query = "access_token=" + accessToken;
+        String url = "https://api.miro.com/v1/boards/"+ boardID +"/widgets/";
+        Charset charset = java.nio.charset.StandardCharsets.UTF_8;
+        try {
+            URLConnection connection = new URL(url + "?" + query).openConnection();
+            connection.setRequestProperty("Accept-Charset", charset.displayName());
+            InputStream response = connection.getInputStream();
+            Scanner scanner = new Scanner(response);
+            String responseBody = scanner.useDelimiter("\\A").next();
+
+            System.out.println(responseBody);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
