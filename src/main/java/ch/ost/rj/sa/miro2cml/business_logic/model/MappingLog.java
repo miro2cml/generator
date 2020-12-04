17bb60ba422c0ed08a969a92cef1c7add89298a7
Timothée Moos <tmoos@hsr.ch>
2020-12-04 14:56:22 +0000

    Resolve "Metadata am Anfang von CMLFile"

--- src/main/java/ch/ost/rj/sa/miro2cml/business_logic/model/MappingLog.java
+++ src/main/java/ch/ost/rj/sa/miro2cml/business_logic/model/MappingLog.java
@@ -7,22 +7,27 @@ import java.util.List;
 
 public class MappingLog {
     private final String boardID;
-    private ArrayList<String> logEntries = new ArrayList<>();
+    private final ArrayList<String> logEntries = new ArrayList<>();
+    private String metaData = "";
 
     public MappingLog(String boardID) {
         this.boardID = boardID;
     }
 
-    public void addDataAccessLogEntries(DataAccessLog dataAccessLog){
+    public void setMetaData(String metaData) {
+        this.metaData = metaData;
+    }
+
+    public void addDataAccessLogEntries(DataAccessLog dataAccessLog) {
         logEntries.addAll(dataAccessLog.getLogEntries());
     }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        builder.append("MappingLog for Board: ").append(boardID).append(System.lineSeparator());
-        for (String entry: logEntries
-             ) {
+        builder.append(metaData);
+        for (String entry : logEntries
+        ) {
             builder.append(entry).append(System.lineSeparator());
         }
         return builder.toString();
@@ -32,21 +37,23 @@ public class MappingLog {
         return logEntries;
     }
 
-    public void addLogEntry(String entry){
+    public void addLogEntry(String entry) {
         logEntries.add(entry);
     }
 
-    public void addInfoLogEntry(String entry){
-        logEntries.add("Info: "+entry);
+    public void addInfoLogEntry(String entry) {
+        logEntries.add("Info: " + entry);
     }
 
-    public void addWarningLogEntry(String entry){
-        logEntries.add("WARNING: "+entry);
+    public void addWarningLogEntry(String entry) {
+        logEntries.add("WARNING: " + entry);
     }
-    public void addSuccessLogEntry(String entry){
-        logEntries.add("Success: "+entry);
+
+    public void addSuccessLogEntry(String entry) {
+        logEntries.add("Success: " + entry);
     }
-    public void addErrorLogEntry(String entry){
-        logEntries.add("ERROR: "+entry);
+
+    public void addErrorLogEntry(String entry) {
+        logEntries.add("ERROR: " + entry);
     }
 }
