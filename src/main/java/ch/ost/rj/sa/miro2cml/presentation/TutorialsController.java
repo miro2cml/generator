c5362db2ed05d2913c508a11beea36422d8eca1b
Timothée Moos <tmoos@hsr.ch>
2020-12-04 15:02:36 +0000

    Resolve "Add Frontent pages for tutorials and supported templates"

---
+++ src/main/java/ch/ost/rj/sa/miro2cml/presentation/TutorialsController.java
@@ -0,0 +1,27 @@
+package ch.ost.rj.sa.miro2cml.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class TutorialsController {
+    @GetMapping(path = "/UserStories")
+    public String getUserStories(Model model) {
+        model.addAttribute("module", "tutorials");
+        return "UserStories";
+    }
+    @GetMapping(path = "/EventStorming")
+    public String getEventStorming(Model model) {
+        model.addAttribute("module", "tutorials");
+        return "EventStorming";
+    }
+
+    @GetMapping(path = "/BoundedContext")
+    public String getBoundedContext(Model model) {
+        model.addAttribute("module", "tutorials");
+        return "BoundedContext";
+    }
+
+}
